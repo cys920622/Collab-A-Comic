54be1a472a855d91fcfb9888ef34273795485413
Daniel Choi <cys920622@gmail.com>
2016-02-08 04:08:53 -0800

    Initial commit: MongoLab hooked up

---
+++ routes/index.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var router = express.Router();
+var PUserSchema = require('mongoose').model('PowerUsers');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  PUserSchema.create({
+    name: 'Daniel Doe',
+    age: 3
+  });
+
+  res.render('index', { title: 'Homepage' });
+  console.log('GET home page');
+
+});
+
+module.exports = router;

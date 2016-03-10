da1864b5356db74d9c2439867560f2b5d514d978
LANSI CHU <chulansi@gmail.com>
2016-03-10 02:59:04 -0800

    Delete panels WIP

--- routes/index.ts
+++ routes/index.ts
@@ -531,19 +531,22 @@ router.post('/user/:profileUsername/subscribers/unsubscribe', isLoggedIn, functi
   res.redirect(req.get('referer'));
 });
 
-// Delete a comic strip
-router.post('/comic/:comicid/remove',function(req,res){
+// Delete a panel from comic strip
+router.post('/comic/:comicid/remove/',function(req,res){
   var cid = req.params.comicid;
-  console.log("Trying to delete "+ cid);
+  console.log('cid: '+cid);
+  var panelloc = req.query.panelloc;
+  console.log('panelloc: '+panelloc);
+  console.log("Trying to delete "+ panelloc);
   Comic.update({_id: cid}, {$pull:
-  { subs: { subscriber: req.user.username
+  { imgarray: { panelloc: panelloc
   }}}, function (err) {
-    if (err) console.log('Error removing subscriber!');
+    if (err) console.log('Error removing panel!');
   });
   Account.update({_id: req.user._id}, {$pull:
-  { subs: { subCid: cid
+  { contributions: { cid: cid
   }}}, function (err) {
-    if (err) console.log('Error removing subscription!');
+    if (err) console.log('Error removing contribution!');
   });
   res.redirect(req.get('referer'));
 });
